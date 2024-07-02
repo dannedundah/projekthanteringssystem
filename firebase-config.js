28c230ccb7b6fb551a732530e6633f7d366c2ee0
Daniel <Daniel@Daniel-Lenovo.FM.local>
2024-07-02 09:52:25 +0200

    Uppdatera firebase-config och läggatillprojekt.js för att inkludera storage

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getFirestore, collection, getDocs, addDoc, updateDoc, doc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDYqBOra2wDjyPweyBGnkVMANsvLOx9pps",
-    authDomain: "projekthanteringsystem.firebaseapp.com",
-    projectId: "projekthanteringsystem",
-    storageBucket: "projekthanteringsystem.appspot.com",
-    messagingSenderId: "87207954816",
-    appId: "1:87207954816:web:167659270c0d6eee901965",
-    measurementId: "G-8HMD30CFYS"
+  apiKey: "AIzaSyDYqBOra2wDjyPweyBGnkVMANsvLOx9pps",
+  authDomain: "projekthanteringsystem.firebaseapp.com",
+  projectId: "projekthanteringsystem",
+  storageBucket: "projekthanteringsystem.appspot.com",
+  messagingSenderId: "87207954816",
+  appId: "1:87207954816:web:167659270c0d6eee901965",
+  measurementId: "G-8HMD30CFYS"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, collection, addDoc, getDocs, ref, uploadBytes, getDownloadURL };
+export { db, collection, getDocs, addDoc, updateDoc, doc, storage, ref, uploadBytes, getDownloadURL };
