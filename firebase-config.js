90e9c347f90544f09251fd9020b6ee94b2fa4c45
Daniel <Daniel@Daniel-Lenovo.FM.local>
2024-07-05 12:59:34 +0200

    Update firebase-config.js with authentication modules and other necessary Firebase services

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getFirestore, collection, getDocs, updateDoc, doc, addDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDYqBOra2wDjyPweyBGnkVMANsvLOx9pps",
@@ -15,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const auth = getAuth(app);
 
-export { db, collection, getDocs, updateDoc, doc, addDoc, getDoc, storage, ref, uploadBytes, getDownloadURL };
+export { db, collection, getDocs, updateDoc, doc, addDoc, getDoc, storage, ref, uploadBytes, getDownloadURL, auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged };
