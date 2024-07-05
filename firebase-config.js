8085a9100bb456fe878239c32a48b69197a042fe
Daniel <Daniel@Daniel-Lenovo.FM.local>
2024-07-05 12:19:29 +0200

    Update firebase-config.js and fix errors in various scripts

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore, collection, getDocs, updateDoc, doc, addDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getFirestore, collection, getDocs, updateDoc, doc, addDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
 
 const firebaseConfig = {
@@ -16,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, collection, getDocs, updateDoc, doc, addDoc, storage, ref, uploadBytes, getDownloadURL };
+export { db, collection, getDocs, updateDoc, doc, addDoc, getDoc, storage, ref, uploadBytes, getDownloadURL };
